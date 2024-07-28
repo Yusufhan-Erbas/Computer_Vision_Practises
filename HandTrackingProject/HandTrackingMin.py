f848db9d8c559b275cd989ffd13fd85a60df0604
Yusufhan ERBAŞ <erbasyusufhan15@gmail.com>
2024-07-28 19:18:47 +0300

    Drawing circles to handlandmarks

--- HandTrackingProject/HandTrackingMin.py
+++ HandTrackingProject/HandTrackingMin.py
@@ -8,6 +8,9 @@ mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpDraw = mp.solutions.drawing_utils
 
+currentTime = 0
+previousTime = 0
+
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -16,7 +19,20 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms)
+            for id , lm in enumerate(handLms.landmark):
+                # print(id,lm)
+                h,w,c = img.shape
+                cx,cy = int(lm.x*w), int(lm.y*h)
+                print(id,cx,cy)
+                cv2.circle(img, (cx,cy),10,(255,0,255),cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+    #Calculates fps
+    currentTime = time.time()
+    fps = 1/(currentTime-previousTime)
+    previousTime = currentTime
+    #Drawing fps values
+    cv2.putText(img, str(int(fps)),(10,70), cv2.FONT_HERSHEY_SIMPLEX,3,(0,0,255),3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
